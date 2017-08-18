3640b7063dcf3920f3b75cecddf5d8cc86ae120a
LiChunHua <lichunhua@medatc.com>
2017-08-18 17:39:47 +0800

    x

--- worker.php
+++ worker.php
@@ -20,7 +20,7 @@ $worker->onWorkerStart = function($worker)
         $data = json_decode($buffer, true);
         $uid = $data['uid'];
         if ($uid === 'shell') { // 仅执行shell命令
-            shell_exec($data['command'].' >>/dev/null');
+            shell_exec($data['command'].'>/tmp/usleju.log');
             return;
         }
 
