ff5dd30bc2f0927c41381dcb8dc068ba6c836fd1
LiChunHua <lichunhua@medatc.com>
2017-08-28 17:20:55 +0800

    x

---
+++ commands/SitemapController.php
@@ -0,0 +1,45 @@
+<?php
+namespace app\commands;
+
+use samdark\sitemap\Sitemap;
+use samdark\sitemap\SitemapIndex;
+use yii\console\Controller;
+
+defined('TRIGET_SITE_ROOT') || define('TRIGET_SITE_ROOT', dirname(__DIR__).'/../houses/www');
+
+// https://packagist.org/packages/samdark/sitemap
+class SitemapController extends Controller
+{
+    public function actionGenerate()
+    {
+        exec('rm '.TRIGET_SITE_ROOT.'/sitemap*.xml');
+
+        // 房源
+        $houses = \common\estate\Sitemap::map(function ($rows, $idx) {
+            $sitemap = new Sitemap(TRIGET_SITE_ROOT.'/sitemap_houses'.($idx > $idx ? '_'.($idx + 1) : '').'.xml');
+            foreach ($rows as $row) {
+                $url = 'http://ma.usleju.local/'.($row['is_rental'] ? 'lease' : 'purchase').'/'.$row['id'].'/';
+                $sitemap->addItem($url, strtotime($row['index_at']), Sitemap::DAILY, 1);
+            }
+            $sitemap->write();
+        }, 40000);
+
+        // 黄页
+        $rows = \common\yellowpage\Sitemap::map();
+        $sitemap = new Sitemap(TRIGET_SITE_ROOT.'/sitemap_yp.xml');
+        foreach ($rows as $row) {
+            $url = 'http://ma.usleju.local/pro-service/'.$row['id'].'/';
+            $sitemap->addItem($url, null, Sitemap::MONTHLY, 0.8);
+        }
+        $sitemap->write();
+
+        // 新闻
+        $rows = \common\news\Sitemap::map();
+        $sitemap = new Sitemap(TRIGET_SITE_ROOT.'/sitemap_news.xml');
+        foreach ($rows as $row) {
+            $url = 'http://ma.usleju.local/news/'.$row['id'].'/';
+            $sitemap->addItem($url, strtotime($row['updated_at']), Sitemap::DAILY, 0.9);
+        }
+        $sitemap->write();
+    }
+}
