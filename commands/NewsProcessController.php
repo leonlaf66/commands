57f299bc7bfa233b53d99460e5a11bf0302db259
LiChunHua <lichunhua@medatc.com>
2018-05-10 08:14:50 +0800

    x

--- commands/NewsProcessController.php
+++ commands/NewsProcessController.php
@@ -16,16 +16,81 @@ class NewsProcessController extends Controller
             ->update('news', ['status' => 2], $where)
             ->execute();
 
-        $content = $db->createCommand('select content from news where '.$where)->queryScalar();
+        $content = $db->createCommand('select content_raw from news where '.$where)->queryScalar();
 
-        // 编译内容
-        $newContent = WS::$app->wxImage->process($content, true);
-        $db->createCommand()
-            ->update('news', ['content' => $newContent], $where)
-            ->execute();
+        // 替换内容中的图片
+        if (preg_match_all('/<img.*?src="(.*?)".*?>/is', $content, $matchs)) {
+            foreach ($matchs[1] as $imageUrl) {
+                $newImageUrl = $this->convertToLocalImageUrl($imageUrl);
+                var_dump($newImageUrl);
+                $content = str_replace($imageUrl, $newImageUrl, $content);
+            }
+        }
 
         $db->createCommand()
-            ->update('news', ['status' => 1], $where)
+            ->update('news', ['content' => $content, 'status' => 1], $where)
             ->execute();
+    }
+
+    public function actionAll()
+    {
+        $db = WS::$app->db;
+        $ids = $db->createCommand('select id from news')->queryColumn();
+
+        $total = count($ids);
+        foreach ($ids as $idx => $id) {
+            $this->actionIndex($id);
+
+            $index = $idx + 1;
+            // echo "{$index}/$total           \r";
+        }
+    }
+
+    protected function convertToLocalImageUrl($remoteUrl)
+    {
+        $hashId = md5($remoteUrl);
+        $localFileDir = sprintf('%s/%s/%s',
+            substr($hashId, 0, 1),
+            substr($hashId, 1, 1),
+            substr($hashId, 2, 2)
+        );
+
+        $rootDir = \WS::$app->params['media']['root'].'/news/img';
+        $fileDir = $rootDir.'/'.$localFileDir;
+
+        if (! is_dir($fileDir)) {
+            mkdir($fileDir, 0777, true);
+            chmod($fileDir, 0777);
+        }
+
+        $localFile = $fileDir.'/'.substr($hashId, 4).'.jpg';
+
+        /*已经有本地缓存*/
+        if (file_exists($localFile)) {
+            return '//'.$localFileDir.'/'.substr($hashId, 4).'.jpg';
+            // return $localFile;
+        }
+
+        $blob = $this->fetchRemoteImage($remoteUrl);
+
+        $fp = fopen($localFile, 'wb');
+        $imgLen = strlen($blob);
+        $_inx = 1024;
+        $_time = ceil($imgLen / $_inx);
+        for($i = 0; $i < $_time; $i ++){
+            fwrite($fp,substr($blob, $i * $_inx, $_inx));
+        }
+        fclose($fp);
+        return '//'.$localFileDir.'/'.substr($hashId, 4).'.jpg';
+    }
+
+    protected function fetchRemoteImage($url)
+    {
+        ob_start();
+        @ readfile($url);
+        $blob = ob_get_contents();
+        ob_end_clean();
+
+        return $blob;
     }
 }
--- commands/SitemapController.php
+++ commands/SitemapController.php
@@ -1,8 +1,8 @@
 <?php
 namespace app\commands;
 
-use libs\Sitemap;
-use samdark\sitemap\SitemapIndex;
+use samdark\sitemap\Sitemap;
+use samdark\sitemap\Index as SitemapIndex;
 use yii\console\Controller;
 
 defined('TRIGET_SITE_ROOT') || define('TRIGET_SITE_ROOT', dirname(__DIR__).'/../houses/www');
@@ -10,66 +10,109 @@ defined('TRIGET_SITE_ROOT') || define('TRIGET_SITE_ROOT', dirname(__DIR__).'/../
 // https://packagist.org/packages/samdark/sitemap
 class SitemapController extends Controller
 {
-    public function actionGenerate()
+    public function actionGenerate($type = 'g')
     {
         $domain = \WS::$app->params['domain'];
-        $xmlFiles = [];
+        $sitemapIndex = new SitemapIndex(TRIGET_SITE_ROOT . '/sitemap_index'.($type === 'g' ? '' : '_'.$type).'.xml');
+        $areaIds = ['ma', 'ny', 'ca', 'ga', 'il'];
 
         // 房源
-        $houses = \common\estate\Sitemap::map(function ($rows, $idx) use ($domain, & $xmlFiles) {
-            $sitemap = new Sitemap(TRIGET_SITE_ROOT.'/sitemap_houses'.($idx > $idx ? '_'.($idx + 1) : '').'.xml');
-            $xmlFiles[] = 'sitemap_houses'.($idx > $idx ? '_'.($idx + 1) : '').'.xml';
+        foreach($areaIds as $areaId) {
+            $houses = \common\estate\Sitemap::map($areaId, function ($rows, $idx) use ($domain, $sitemapIndex, $type, $areaId) {
+                $xmlFile = 'sitemap_houses_'.$areaId.($type === 'g' ? '' : '_'.$type).($idx > 0 ? '_'.($idx + 1) : '').'.xml';
+                $sitemap = new Sitemap(TRIGET_SITE_ROOT.'/'.$xmlFile, $type === 'g');
 
-            foreach ($rows as $row) {
-                $url = 'http://ma'.$domain.'/'.($row['is_rental'] ? 'lease' : 'purchase').'/'.$row['id'].'/';
-                $zhUrl = 'http://ma'.$domain.'/zh/'.($row['is_rental'] ? 'lease' : 'purchase').'/'.$row['id'].'/';
-                $sitemap->addItem([$url, $zhUrl], strtotime($row['index_at']), Sitemap::DAILY, 1);
+                foreach ($rows as $row) {
+                    $path = ($row['prop_type'] === 'RN' ? 'lease' : 'purchase').'/'.$row['list_no'].'/';
+
+                    if ($type === 'g') {
+                        $url = 'http://'.$areaId.$domain.'/'.$path;
+                        $zhUrl = 'http://'.$areaId.$domain.'/zh/'.$path;
+
+                        $sitemap->addItem(['en'=>$url, 'zh'=>$zhUrl], strtotime($row['index_at']), Sitemap::DAILY, 1);
+                    } else {
+                        $url = 'http://'.$areaId.$domain.'/zh/'.$path;
+
+                        $sitemap->addItem($url, strtotime($row['index_at']), Sitemap::DAILY, 1);
+                    }
+                }
+
+                $sitemap->write();
+
+                $sitemapIndex->addSitemap('http://'.$areaId.$domain.'/'.$xmlFile);
+            }, ($type === 'g' ? 20000 : 40000));
+        }
+
+        /*学区(仅ma)*/
+        $xmlFile = 'sitemap_yp_ma_xq'.($type === 'g' ? '' : '_'.$type).'.xml';
+        $sitemap = new Sitemap(TRIGET_SITE_ROOT.'/'.$xmlFile, $type === 'g');
+        $xqids = \WS::$app->db->createCommand('select id from schooldistrict')
+            ->queryColumn('id');
+        foreach ($xqids as $xqid) {
+            if ($type === 'g') {
+                $url = 'http://ma'.$domain.'/school-district/'.$xqid.'/';
+                $zhUrl = 'http://ma'.$domain.'/zh/school-district/'.$xqid.'/';
+                $sitemap->addItem(['en'=>$url, 'zh'=>$zhUrl], null, Sitemap::MONTHLY, 0.8);
+            } else {
+                $url = 'http://ma'.$domain.'/school-district/zh/'.$xqid.'/';
+                $sitemap->addItem($url, null, Sitemap::MONTHLY, 0.8);
             }
+        }
 
-            exec('rm '.TRIGET_SITE_ROOT.'/sitemap_houses*.xml');
+        if (count($xqids) > 0) {
             $sitemap->write();
-        }, 40000);
+            $sitemapIndex->addSitemap('http://ma'.$domain.'/'.$xmlFile);
+        }
 
         // 黄页
-        $rows = \common\yellowpage\Sitemap::map();
-        $sitemap = new Sitemap(TRIGET_SITE_ROOT.'/sitemap_yp.xml');
-        $xmlFiles[] = 'sitemap_yp.xml';
-        foreach ($rows as $row) {
-            $url = 'http://ma'.$domain.'/pro-service/'.$row['id'].'/';
-            $zhUrl = 'http://ma'.$domain.'/zh/pro-service/'.$row['id'].'/';
-            $sitemap->addItem([$url, $zhUrl], null, Sitemap::MONTHLY, 0.8);
-        }
+        foreach($areaIds as $areaId) {
+            $xmlFile = 'sitemap_yp_'.$areaId.($type === 'g' ? '' : '_'.$type).'.xml';
 
-        exec('rm '.TRIGET_SITE_ROOT.'/sitemap_yp.xml');
-        $sitemap->write();
+            $rows = \common\yellowpage\Sitemap::map($areaId);
+            $sitemap = new Sitemap(TRIGET_SITE_ROOT.'/'.$xmlFile, $type === 'g');
+            foreach ($rows as $row) {
+                if ($type === 'g') {
+                    $url = 'http://'.$areaId.$domain.'/pro-service/'.$row['id'].'/';
+                    $zhUrl = 'http://'.$areaId.$domain.'/zh/pro-service/'.$row['id'].'/';
+                    $sitemap->addItem(['en'=>$url, 'zh'=>$zhUrl], null, Sitemap::MONTHLY, 0.8);
+                } else {
+                    $url = 'http://'.$areaId.$domain.'/zh/pro-service/'.$row['id'].'/';
+                    $sitemap->addItem($url, null, Sitemap::MONTHLY, 0.8);
+                }
+            }
 
-        // 新闻
-        $rows = \common\news\Sitemap::map();
-        $sitemap = new Sitemap(TRIGET_SITE_ROOT.'/sitemap_news.xml');
-        $xmlFiles[] = 'sitemap_news.xml';
-        foreach ($rows as $row) {
-            $url = 'http://ma'.$domain.'/news/'.$row['id'].'/';
-            $sitemap->addItem($url, strtotime($row['updated_at']), Sitemap::DAILY, 0.9);
-        }
-        exec('rm '.TRIGET_SITE_ROOT.'/sitemap_news.xml');
-        $sitemap->write();
-
-        // 写入robots文件
-        $robotsContent = file_get_contents(TRIGET_SITE_ROOT.'/robots.txt');
-        
-        if (preg_match_all('/Sitemap:.*/', $robotsContent, $matches)) {
-            foreach ($matches[0] as $url) {
-                $robotsContent = str_replace($url, '', $robotsContent);
+            if (count($rows) > 0) {
+                $sitemap->write();
+                $sitemapIndex->addSitemap('http://'.$areaId.$domain.'/'.$xmlFile);
             }
         }
-        $robotsContent = trim($robotsContent);
-        $robotsContent .= "\n";
-        foreach($xmlFiles as $xmlFilename) {
-            $robotsContent .= "Sitemap: http://ma{$domain}/{$xmlFilename}\n";
+
+        // 新闻
+        foreach($areaIds as $areaId) {
+            $xmlFile = 'sitemap_news_'.$areaId.($type === 'g' ? '' : '_'.$type).'.xml';
+
+            $rows = \common\news\Sitemap::map($areaId, $type === 'g');
+            $sitemap = new Sitemap(TRIGET_SITE_ROOT.'/'.$xmlFile);
+
+            foreach ($rows as $row) {
+                $url = 'http://'.$areaId.$domain.'/zh/news/'.$row['id'].'/';
+                $sitemap->addItem($url, strtotime($row['updated_at']), Sitemap::DAILY, 0.9);
+            }
+
+            if (count($rows) > 0) {
+                $sitemap->write();
+                $sitemapIndex->addSitemap('http://'.$areaId.$domain.'/'.$xmlFile);
+            }
         }
-        file_put_contents(TRIGET_SITE_ROOT.'/robots.txt', $robotsContent);
+
+        $sitemapIndex->write();
 
         /*log*/
         file_put_contents(__DIR__.'/../log.log', date('Y-m-d H:i:s').' sitemap xml'."\n", FILE_APPEND);
+    }
+
+    public function actionGenerateBaidu($type = 'b')
+    {
+        return $this->actionGenerate($type);
     }
 }
